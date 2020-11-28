d2bb8503dc519814f773757f49c98793ef2be9a4
sathishVM <sathishvm37@gmail.com>
2020-11-28 05:30:47 +0530

    ✍️ Created limiter using express-rate-limit

---
+++ src/middlewares/rateLimiter.ts
@@ -0,0 +1,11 @@
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20,
+  message:
+    'Too many requests from this IP, please try again later after an hour.',
+  skipSuccessfulRequests: true,
+});
+
+export { limiter };
